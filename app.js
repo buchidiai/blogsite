de48c209427ee834848410182cffc653d2291294
54 Bridge <54bridgeapp@gmail.com>
2020-01-06 13:38:39 -0600

    added route params

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -43,6 +43,20 @@ app.post("/compose", (req, res) => {
   };
   posts.push(post);
   res.redirect("/");
+});
+
+app.get("/posts/:title", (req, res) => {
+  const param = _.lowerCase(req.params.title);
+
+  posts.forEach(element => {
+    const savedPosts = _.lowerCase(element.title);
+    console.log(param, savedPosts);
+
+    if (param == savedPosts) {
+      console.log("found");
+      res.render("post", { content: element });
+    }
+  });
 });
 
 const port = 3000;
